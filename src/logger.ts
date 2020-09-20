d6c2203acf90a3775270194d6f517167002cd5f8
Matthew Lee <matthew.lee711@gmail.com>
2020-09-20 01:02:43 -0500

    Logger mods

--- src/logger.ts
+++ src/logger.ts
@@ -20,11 +20,6 @@ export const Logger = winston.createLogger({
 		prettyJson
 	),
 	transports: [
-		new winston.transports.Console({}),
-		new (winston.transports.File)({
-			name: 'info-file',
-			filename: './logs/info-file.log',
-			level: 'info'
-		})
+		new winston.transports.Console({})
 	]
 });
